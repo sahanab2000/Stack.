26b1865df066fb0073c5cd68a2686b575acfa10a
sahanab2000 <85185990+sahanab2000@users.noreply.github.com>
2022-11-13 12:51:25 +0530

    Add files via upload

---
+++ stack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{ 
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to C# Collections");
+            doStackDemo();
+        }
+        private static void doStackDemo()
+        {
+            Console.WriteLine("\nIn doStackDeno"); //Creating Stack
+            Stack<string> stack = new Stack<string>();
+            stack.Push("Sneha");
+            stack.Push("Kiran");
+            stack.Push("Gowri");
+            stack.Push("Mahitha");
+            stack.Push("granth");
+            string pop = stack.Pop();
+
+            // Iterating stack elements
+            foreach (var element in stack)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("Poped element: " + pop);
+        }
+       
+    }
+}
